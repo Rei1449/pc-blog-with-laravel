eadf86f0d15f70fbb961bfc01493bde137f3e338
Rei1449 <kiriya4673@gmail.com>
2023-07-13 13:20:35 +0900

    マイページへの遷移をnavに追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,12 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import { library, dom } from '@fortawesome/fontawesome-free';
+
+library.add(fasFaHeart);
+dom.watch();
+
+
 window.Alpine = Alpine;
 
 Alpine.start();
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
+                        {{ __('マイページ') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
