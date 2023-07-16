2d099a375e5d5912f44048349dc311c76be48252
Rei1449 <kiriya4673@gmail.com>
2023-07-16 19:23:55 +0900

    ランキング実装+web.phpのログイン周りを修正

--- routes/web.php
+++ routes/web.php
@@ -19,35 +19,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', [PostController::class, 'home'])->name('home');
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('posts/{post}/comment',[CommentController::class, 'comment']);
-Route::post('/comments', [CommentController::class, 'store']);
+Route::get('/', [PostController::class, 'home'])->name('home');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-
-Route::post('/posts/{post}/bookmark', [BookmarkController::class, 'store'])->name('bookmark.store');
-Route::delete('/posts/{post}/unbookmark', [BookmarkController::class, 'destroy'])->name('bookmark.destroy');
-Route::get('/bookmarks', [PostController::class, 'bookmark_posts'])->name('bookmarks');
-
-Route::post('/posts/{post}/like', [LikeController::class, 'store'])->name('like.store');
-Route::delete('/posts/{post}/unlike', [LikeController::class, 'destroy'])->name('like.destroy');
-Route::get('/likes', [PostController::class, 'like_posts'])->name('likes');
-
 Route::get('/search', [SearchController::class, 'index'])->name('search');
-
 Route::get('/ranking', [PostController::class, 'ranking'])->name('ranking');
 
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/posts/create', [PostController::class, 'create']);
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::get('posts/{post}/comment',[CommentController::class, 'comment']);
+    Route::post('/comments', [CommentController::class, 'store']);
+    
+    Route::post('/posts/{post}/bookmark', [BookmarkController::class, 'store'])->name('bookmark.store');
+    Route::delete('/posts/{post}/unbookmark', [BookmarkController::class, 'destroy'])->name('bookmark.destroy');
+    Route::get('/bookmarks', [PostController::class, 'bookmark_posts'])->name('bookmarks');
+    
+    Route::post('/posts/{post}/like', [LikeController::class, 'store'])->name('like.store');
+    Route::delete('/posts/{post}/unlike', [LikeController::class, 'destroy'])->name('like.destroy');
+    Route::get('/likes', [PostController::class, 'like_posts'])->name('likes');
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
